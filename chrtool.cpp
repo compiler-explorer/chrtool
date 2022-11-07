c7bb0f47212cb4a75e530a0462d57a6f10148379
partouf <partouf@gmail.com>
2022-11-07 13:05:50 +0100

    typefix

--- chrtool.cpp
+++ chrtool.cpp
@@ -31,7 +31,7 @@ std::string charToBin(unsigned char c)
 
 void printbuf(char *buffer, size_t len)
 {
-    for (int i = 0; i < len; i++)
+    for (size_t i = 0; i < len; i++)
     {
         if (i % 8 == 0)
         {
